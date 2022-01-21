e1b3d9d98f2f4b3d7605f27147e5f45668959417
Ritika Patel <72975134+Ritika-patel@users.noreply.github.com>
2022-01-21 16:11:51 +0530

    Create findKthPositive.cpp

---
+++ Leetcode/findKthPositive.cpp
@@ -0,0 +1,25 @@
+//brute-force
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n=arr.size();
+        map<int, int>mpp;
+        int count=0;
+        int num=0;
+        
+        for(int i=0; i<n; i++){
+            mpp[arr[i]]++;
+        }
+        
+        for(int i=1; i<=2000; i++){
+            if(mpp.find(i)==mpp.end()){
+                num=i;
+                count++;
+            }
+            if(count==k){
+                return num;
+            }
+        }
+        return num;
+    }
+};
