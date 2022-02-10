ca6a04206798726554781bbccecc1b8aab410cd9
Ritika Patel <72975134+Ritika-patel@users.noreply.github.com>
2022-02-10 23:59:36 +0530

    Create preorder.cpp

---
+++ Leetcode/BST/preorder.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//preorder traversal - root left right
+class Solution {
+public:
+    void preorder(TreeNode* root, vector<int> &vec){
+        if(root==NULL) return;
+        vec.push_back(root->val);
+        preorder(root->left, vec);
+        preorder(root->right, vec);
+    }
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>vec;
+        preorder(root, vec);
+        return vec;
+    }
+};
