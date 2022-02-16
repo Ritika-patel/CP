58fb9c83e02bf42a5db03856577a47e315c96ae2
Ritika Patel <72975134+Ritika-patel@users.noreply.github.com>
2022-02-17 01:23:34 +0530

    Update uniquePaths.cpp

--- DP/codestudio/uniquePaths.cpp
+++ DP/codestudio/uniquePaths.cpp
@@ -56,3 +56,30 @@ int uniquePaths(int m, int n) {
 }
 
 //SPACE OPTIMIZATION
+int uniquePaths(int m, int n) {
+    vector<int> prev(n,0);
+    for(int i=0; i<m; i++){
+        vector<int> temp(n,0);
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0){
+                temp[j]=1;
+                continue;
+            }
+            
+            int up=0;
+            int left =0;
+            
+            if(i>0)
+                up = prev[j];
+            if(j>0)
+                left = temp[j-1];
+                
+            temp[j] = up + left;
+        }
+        prev = temp;
+    }
+    
+    return prev[n-1];
+    
+}
+
