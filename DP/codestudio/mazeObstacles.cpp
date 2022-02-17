7dbff66d1504694645b4ca512485a81bd57ff940
Ritika Patel <72975134+Ritika-patel@users.noreply.github.com>
2022-02-17 12:26:48 +0530

    Update mazeObstacles.cpp

--- DP/codestudio/mazeObstacles.cpp
+++ DP/codestudio/mazeObstacles.cpp
@@ -48,3 +48,25 @@ int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
     }
     return dp[n-1][m-1];
 }
+//SPACE OPTIMIZATION
+int mod=(int)(1e9 + 7);
+int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
+    // Write your code here
+    //array of row size which has m columns
+    vector<int>prev(m, 0);
+    for(int i=0; i<n; i++){
+            vector<int>curi(m, 0);
+        for(int j=0; j<m; j++){
+            if(mat[i][j]==-1) curi[j]=0;
+            else if(i==0 && j==0) curi[j]=1;
+            else{
+                int up=0, left=0;
+                if(i>0) up=prev[j];
+                if(j>0) left=curi[j-1];
+                curi[j]=(left+up)%mod;
+            }
+        }
+        prev=curi;
+    }
+    return prev[m-1];
+}
