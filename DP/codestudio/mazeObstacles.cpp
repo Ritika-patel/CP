951b1746b721c646090225ffa2e9cdc2e5d05592
Ritika Patel <72975134+Ritika-patel@users.noreply.github.com>
2022-02-17 11:37:44 +0530

    Update mazeObstacles.cpp

--- DP/codestudio/mazeObstacles.cpp
+++ DP/codestudio/mazeObstacles.cpp
@@ -11,3 +11,20 @@ int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
     // Write your code here
     solve(n-1, m-1, mat);
 }
+
+//MEMOIZATION SOLUTION
+int solve(int n, int m, vector<vector<int>>&dp, vector<vector<int>>&mat){
+    int up=0, left=0;
+    if(mat[n][m]==-1) return 0;
+    if(dp[n][m]!=-1) return dp[n][m];
+    if(m==0 && n==0) return 1;
+    if(m<0 || n<0) return 0;
+    if(n>=1)up=solve(n-1, m, dp, mat);
+    if(m>=1)left=solve(n, m-1, dp, mat);
+    return dp[n][m]=up+left;
+}
+int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
+	// Write your code here.
+    vector<vector<int>> dp( n , vector<int> (m, -1));
+    return solve(n-1, m-1, dp, mat);
+}
